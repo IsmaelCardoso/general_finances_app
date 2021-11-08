b53bfe46d1d503539cefaf0fb09fd151d685c83d
ismaelcardoso <ismael.cardosos@gmail.com>
2021-11-08 08:51:25 -0300

    chore(Dashboard and Global styles):  - Created component Dashboard  - Created components with styled components  - Created GLobal styles  - Create provider styles  - Defined cutom type in styled components

---
+++ src/screens/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Container, Title} from './styles'
+
+const Dashboard = () => {
+    return (
+    <Container>
+        <Title>Dashboards</Title>
+    </Container>
+    );
+}
+
+export default Dashboard;
