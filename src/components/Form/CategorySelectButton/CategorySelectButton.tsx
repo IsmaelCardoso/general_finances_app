e902dd4d78769edd93d120f4f0e8ac32e7fd216e
ismaelcardoso <ismael.cardosos@gmail.com>
2021-11-19 17:36:09 -0300

    fix(CategorySelect):  Renamed component

---
+++ src/components/Form/CategorySelectButton/CategorySelectButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { TouchableOpacityProps } from "react-native";
+
+import { Container, Category, Icon } from "./styles";
+
+interface Props extends TouchableOpacityProps {
+    title: string;
+    onPress: () => void;
+}
+
+const CategorySelectButton = ({ title, onPress }: Props) => {
+    return (
+        <Container onPress={onPress}>
+            <Category>{title}</Category>
+            <Icon name="chevron-down" />
+        </Container>
+    );
+};
+
+export default CategorySelectButton;
