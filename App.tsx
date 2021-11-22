45621ffc8116f8ec6784f66ac935e6251c056525
ismaelcardoso <ismael.cardosos@gmail.com>
2021-11-22 09:25:49 -0300

    chore(InputForm, Register and app.routes):  - Install @react-navigation/bottom-tabs  - Install @react-navigation/native  - Created app.routes  - Implemented app.routes -> App

--- App.tsx
+++ App.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading'
-import {ThemeProvider} from 'styled-components'
+import React from "react";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
+
+import { NavigationContainer } from "@react-navigation/native";
+import AppRoutes from "./src/routes/app.routes";
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold,
-} from '@expo-google-fonts/poppins'
+} from "@expo-google-fonts/poppins";
 
-import theme from './src/global/styles/theme'
-import Dashboard from './src/screens/Dashboard';
+import theme from "./src/global/styles/theme";
+import Register from "./src/screens/Register";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
-    Poppins_700Bold
+    Poppins_700Bold,
   });
 
-  if(!fontsLoaded) {
-    return <AppLoading />
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
-  )
+  );
 }
